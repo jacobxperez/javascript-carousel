555fcd4f7c5ffa9f7cf52bff97c8a4dbf562bdd9
Jacob <jacobxperez@gmx.com>
2023-04-08 21:49:45 -0600

    Here's a refactored version of the code that is simplified and more concise:
    The controlsContainer parameter in the constructor has been renamed to controlsSelector for consistency with the rest of the class.
    The images and imgCache properties have been removed since they are no longer used.
    The preloadInitialImages() method has been updated to only preload the first image in each slide, if it exists.
    The cycleItems() method has been renamed to cycleSlides()

--- js/carousel.js
+++ js/carousel.js
@@ -8,34 +8,31 @@ class Carousel {
     constructor(
         sliderSelector,
         slideSelector,
-        controlsContainer = '[data-controls]'
+        controlsSelector = '[data-controls]'
     ) {
         this.slider = document.querySelector(sliderSelector);
         this.slides = this.slider.querySelectorAll(slideSelector);
-        this.controlsContainer = this.slider.querySelector(controlsContainer);
-        this.tabs = this.slider.querySelectorAll('[data-tab]');
-        this.images = this.slider.querySelectorAll('img');
-        this.imgCache = [];
-        this.currIndex = 0;
-        this.sliderInterval;
+        this.controls = this.slider.querySelector(controlsSelector);
+        this.tabs = this.controls.querySelectorAll('[data-tab]');
+        this.currentIndex = 0;
         this.intervalTime = 5000;
-        this.lazyLoadThreshold = 2; // number of slides to preload initially
+        this.lazyLoadThreshold = 2;
         this.initialize();
     }
 
     initialize() {
         this.preloadInitialImages().then(() => {
-            this.cycleItems();
-            if (this.controlsContainer) {
-                this.controlsContainer.addEventListener(
+            this.cycleSlides();
+            if (this.controls) {
+                this.controls.addEventListener(
                     'click',
-                    this.controls.bind(this)
+                    this.handleControls.bind(this)
                 );
             }
             if (this.tabs) {
-                this.tabs.forEach((tab, index) => {
-                    tab.setAttribute('data-index', index);
-                });
+                this.tabs.forEach((tab, index) =>
+                    tab.setAttribute('data-index', index)
+                );
             }
         });
     }
@@ -44,103 +41,88 @@ class Carousel {
         const promises = [];
         for (
             let i = 0;
-            i < this.lazyLoadThreshold && i < this.images.length;
+            i < this.lazyLoadThreshold && i < this.slides.length;
             i++
         ) {
-            const image = this.images[i];
-            const imgPromise = new Promise((resolve) => {
-                const img = new Image();
-                img.src = `${image.src}`;
-                img.onload = resolve;
-            });
-            promises.push(imgPromise);
+            const image = this.slides[i].querySelector('img');
+            if (image) {
+                const imgPromise = new Promise((resolve) => {
+                    const img = new Image();
+                    img.src = image.src;
+                    img.onload = resolve;
+                });
+                promises.push(imgPromise);
+            }
         }
         return Promise.all(promises);
     }
 
-    preloadNextImage() {
-        const nextIndex =
-            (this.currIndex + this.lazyLoadThreshold) % this.slides.length;
-        if (nextIndex < this.images.length) {
-            const image = this.images[nextIndex];
-            const imgPromise = new Promise((resolve) => {
-                const img = new Image();
-                img.src = `${image.src}`;
-                img.onload = resolve;
+    cycleSlides() {
+        const currentSlide = this.slides[this.currentIndex];
+        currentSlide.setAttribute('data-state', 'current');
+        requestAnimationFrame(() => {
+            this.slides.forEach((slide) => {
+                if (slide !== currentSlide) {
+                    slide.removeAttribute('data-state');
+                }
             });
-            this.imgCache.push(imgPromise);
-        }
+        });
     }
 
-    controls(e) {
+    handleControls(e) {
         const target = e.target;
         if (target.matches('[data-button="next-slide"]')) {
             this.changeSlide('next');
-            this.pause();
-            this.preloadNextImage();
+            this.resume();
         } else if (target.matches('[data-button="prev-slide"]')) {
             this.changeSlide('prev');
-            this.pause();
+            this.resume();
         } else if (target.matches('[data-tab]')) {
             this.pause();
-            this.currIndex = target.getAttribute('data-index');
-            this.cycleItems();
+            this.currentIndex = Number(target.getAttribute('data-index'));
+            this.cycleSlides();
         }
-    }
-
-    cycleItems() {
-        const currSlide = this.slides[this.currIndex];
-        this.slides[this.currIndex].setAttribute('data-state', 'current');
-        requestAnimationFrame(() => {
-            for (const slide of this.slides) {
-                if (slide !== currSlide) {
-                    slide.removeAttribute('data-state');
-                }
-            }
-        });
     }
 
     changeSlide(direction) {
         if (direction === 'next') {
-            this.currIndex += 1;
-            if (this.currIndex > this.slides.length - 1) {
-                this.currIndex = 0;
+            this.currentIndex++;
+            if (this.currentIndex > this.slides.length - 1) {
+                this.currentIndex = 0;
             }
         } else if (direction === 'prev') {
-            this.currIndex -= 1;
-            if (this.currIndex < 0) {
-                this.currIndex = this.slides.length - 1;
+            this.currentIndex--;
+            if (this.currentIndex < 0) {
+                this.currentIndex = this.slides.length - 1;
             }
         }
-        this.cycleItems();
+        this.cycleSlides();
     }
 
-    resume() {
-        this.start(this.intervalTime);
+    start(intervalTime = this.intervalTime) {
+        this.interval = setInterval(() => {
+            this.changeSlide('next');
+        }, intervalTime);
         return this;
     }
 
     pause() {
-        clearInterval(this.sliderInterval);
+        clearInterval(this.interval);
+        return this;
+    }
+
+    resume() {
+        this.pause().start();
         return this;
     }
 
     stop() {
-        clearInterval(this.sliderInterval);
-        return this;
-    }
-
-    start(time) {
-        this.sliderInterval = setInterval(() => {
-            this.changeSlide('next');
-            this.preloadNextImage();
-        }, time || this.intervalTime);
+        this.pause();
+        this.currentIndex = 0;
+        this.cycleSlides();
         return this;
     }
 }
 
 // Example usage
-const fullScreenCarousel = new Carousel(
-    '[data-carousel]',
-    '[data-slide]'
-).start();
+const carousel = new Carousel('[data-carousel]', '[data-slide]').start();
